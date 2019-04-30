7e079347a8a93f0cd06e8845c46f9e3f5403dc21
Bruno <brunolobell2@gmail.com>
2019-04-30 20:03:19 -0300

    Uma  mensagem significativa

--- src/operations/operations.cpp
+++ src/operations/operations.cpp
@@ -12,5 +12,9 @@ namespace operations {
     types::Vector2D vectors_sum(types::Vector2D v1, types::Vector2D v2) {
         return types::Vector2D(v1.x + v2.x, v1.y + v2.y);
     }
+
+    types::Vector2D vector_unit(types::Vector2D v1){
+        return types::Vector2D(v1.x/sqrt(pow(v1.x,2) + pow(v1.y,2)), v1.y/sqrt(pow(v1.x,2) + pow(v1.y,2)));
+    }
 }
 }
